9c650157f686887fbb529acca27abc95784e5906
DimitrisLinarakis <92636147+DimitrisLinarakis@users.noreply.github.com>
2023-10-09 22:47:04 +0300

    Value Inc. Project - Data Cleaned Successfully

--- valueInc_sales.py
+++ valueInc_sales.py
@@ -67,10 +67,63 @@ data.iloc[0:3] #views the first 3 rows
 data.iloc[-5:] #views the last 5 rows
 
 data.head() #by default returns the first 5 rows
-
 data.iloc[:,2] #views all rows - column with index = 2
-
 data.iloc[4,2] #views specific value
+
+#using split to split the client's keywords field
+#new_var = column.str.split('sep' , expand = True) ---> splits every single comma, not only the first one
+split_col = data['ClientKeywords'].str.split(',' , expand=True)
+
+#creating new columns for the split columns in Client Keyword
+data['ClientAge'] = split_col[0]
+data['ClientType'] = split_col[1]
+data['LengthOfContract'] = split_col[2]
+
+#drop the brackets, replace function
+data['ClientAge'] = data['ClientAge'].str.replace('[' , '')
+data['LengthOfContract'] = data['LengthOfContract'].str.replace(']' , '')
+
+#using the lower function to change item to lowercase
+data['ItemDescription'] = data['ItemDescription'].str.lower()
+
+#how to merge files
+#bringing in a new dataset
+seasons = pd.read_csv('value_inc_seasons.csv', sep=';')
+
+#merging files: merge_df = pd.merge(df_old, df_new, on ='key')
+data = pd.merge(data, seasons, on= 'Month')
+
+#Drop extra columns from the dataset
+data = data.drop(columns=['ClientKeywords'])
+
+data = data.drop(columns=['Day'])
+data = data.drop(columns=['Month'])
+data = data.drop(columns=['Year'])
+
+#alternative way to drop columns 
+#data = data.drop(columns=['Day','Month','Year'])
+
+#Export into a csv file
+data.to_csv('ValueInc_Cleaned.csv', index = False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
